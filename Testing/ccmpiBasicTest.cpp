1e6e19d8a81ce0cb184436eee0ec1a77558f6229
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-03-12 17:53:52 +0000

    Add Asynchronous Send example.

--- Testing/ccmpiBasicTest.cpp
+++ Testing/ccmpiBasicTest.cpp
@@ -31,7 +31,7 @@ TEST_CASE( "Initialisation Test", "[MPI]" ) {
   std::cout << "Processor " << rank << " of " << size << " says hello\n";
 }
 
-TEST_CASE( "Send String", "[MPI]" ) {
+TEST_CASE( "Synchronous Send String", "[MPI]" ) {
 
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -53,5 +53,45 @@ TEST_CASE( "Send String", "[MPI]" ) {
     REQUIRE(error ==  MPI_SUCCESS);
     CHECK(std::string(buffer) == peace);
     std::cout << "Received message:" << peace << std::endl;
+  }
+}
+
+
+TEST_CASE( "Asynchronous Send String", "[MPI]" ) {
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  // This demo requires at least 2 processes.
+  REQUIRE(size >= 2);
+
+  std::string peace = "I come in peace!";
+  if (rank == 0) {
+    MPI_Request request;
+    int error = MPI_Isend((void*) peace.c_str(), peace.size() + 1, MPI_CHAR, 1, 42, MPI_COMM_WORLD, &request);
+    // We do not guarantee that Rank 1 has received the message yet
+    // We can carry on, and ANY WORK WE DO NOW WILL OVERLAP WITH THE
+    // COMMUNICATION
+
+    // BUT, we can't safely change the string.
+
+    REQUIRE(error ==  MPI_SUCCESS);
+
+    // Do some expensive work here
+    for (int i=0; i<1000; i++) {}; // BUSYNESS FOR EXAMPLE
+
+    MPI_Status status;
+    error = MPI_Wait(&request, &status);
+    REQUIRE(error ==  MPI_SUCCESS);
+
+    // Here, we run code that requires the message to have been
+    // successfully sent.
+  }
+  if (rank == 1) {
+    char buffer[256];
+    int const error = MPI_Recv(buffer, 256, MPI_CHAR, 0, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    REQUIRE(error ==  MPI_SUCCESS);
+    CHECK(std::string(buffer) == peace);
   }
 }
